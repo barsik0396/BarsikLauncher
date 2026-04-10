67a9a34331656e9f3f8e92a61733f991bbe5f84c
barsik0396 <Barsik0396@outlook.com>
2026-04-10 15:44:29 +0300

    Добавил загрузчик для electron-проги

---
+++ docs/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  Menu.setApplicationMenu(null);
+  win.loadFile('index.html');
+}
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
